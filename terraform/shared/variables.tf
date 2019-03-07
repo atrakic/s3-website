510263d7bc5976021582485ec2b6099534b612f7
Admir Trakic <xomodo@gmail.com>
2019-03-07 21:37:52 +0100

    Update bucket name

--- terraform/shared/variables.tf
+++ terraform/shared/variables.tf
@@ -1,5 +1,6 @@
 variable "environment" {}
 variable "aws_region"  { default = "eu-west-1" }
-variable "bucket"      { default = "trakic.io" }
+variable "bucket"      { default = "trakic.io.circleci" }
+
 variable "create_defaults" { default = false   }
 variable "create_iam_user" { default = false   }
